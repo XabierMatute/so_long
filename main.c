16a962180e6ed82ce4210bd9e8af0edb2fd58025
Xabier Matute Rusinol <xmatute-@c3r6s3.42urduliz.com>
2022-12-01 11:41:12 +0100

    añadido la condición del numero de imputs al  main en el main.c pero bien

--- main.c
+++ main.c
@@ -6,13 +6,13 @@
 /*   By: xmatute- <xmatute-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/01 11:26:47 by xmatute-          #+#    #+#             */
-/*   Updated: 2022/12/01 11:39:17 by xmatute-         ###   ########.fr       */
+/*   Updated: 2022/12/01 11:40:42 by xmatute-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int main(int argc, char const *argv[])
 {
-	if (argc != )
-		error_in();
+	if (argc != 2)
+		error_argc();
 	return 0;
 }
