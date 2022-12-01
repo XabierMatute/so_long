311cad17b992870dac2dacb7c5f364daa1ba13ee
Xabier Matute Rusinol <xmatute-@c3r6s3.42urduliz.com>
2022-12-01 15:05:35 +0100

    error_argc terminado

--- Errors/error_argc.c
+++ Errors/error_argc.c
@@ -6,11 +6,15 @@
 /*   By: xmatute- <xmatute-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/01 12:15:40 by xmatute-          #+#    #+#             */
-/*   Updated: 2022/12/01 12:16:59 by xmatute-         ###   ########.fr       */
+/*   Updated: 2022/12/01 13:16:40 by xmatute-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+int				ft_printf(char const *s, ...);
+
 int	error_argc(int argc)
 {
-	
+	ft_printf("Error: has introducido %i argumentos,", argc - 1);
+	ft_printf(" introduce por favor un unico argumento (mapname.ber)\n");
+	return (-1);
 }
