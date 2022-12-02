49348992d13f9c34695bbea952ebcab48be125c5
Xabier Matute Rusinol <xmatute-@c3r6s3.42urduliz.com>
2022-12-02 18:13:05 +0100

    arreglado prototipo de  Errors/error_unreadable.c

--- Errors/error_unreadable.c
+++ Errors/error_unreadable.c
@@ -6,14 +6,14 @@
 /*   By: xmatute- <xmatute-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/02 17:50:23 by xmatute-          #+#    #+#             */
-/*   Updated: 2022/12/02 18:02:44 by xmatute-         ###   ########.fr       */
+/*   Updated: 2022/12/02 18:09:37 by xmatute-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int				ft_printf(char const *s, ...);
 void 			exit(int status);
 
-void	error_unreadable(ber)
+void	error_unreadable(char	*ber)
 {
 	ft_printf("Error: el archivo %s no se ha podido leer\n", ber);
 	exit(-1);
