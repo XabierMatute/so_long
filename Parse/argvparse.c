8e7d63b4fba631f9c6bd5f11c1248f22f9cbc292
Xabier Matute Rusinol <xmatute-@c3r6s3.42urduliz.com>
2022-12-02 17:12:08 +0100

    arreglada una cosa de lo de mirar si .ber

--- Parse/argvparse.c
+++ Parse/argvparse.c
@@ -6,12 +6,20 @@
 /*   By: xmatute- <xmatute-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/01 15:43:35 by xmatute-          #+#    #+#             */
-/*   Updated: 2022/12/02 16:35:04 by xmatute-         ###   ########.fr       */
+/*   Updated: 2022/12/02 17:11:14 by xmatute-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft/libft.h"
 int	error_dotber(char *argv);
+
+// void	berparse(char *ber)
+// {
+// 	if (!valitber(ber))
+// 		error_invalidber(ber);
+	
+// }
+
 
 int		dotber(char	*argv)
 {
@@ -19,7 +27,7 @@ int		dotber(char	*argv)
 	{
 		if (ft_strlen(argv) < 4)
 			return (0);
-		if (!ft_strncmp(argv + 4 - ft_strlen(argv), ".ber", 4))
+		if (!ft_strncmp(argv + (ft_strlen(argv) - 4), ".ber", 4))
 			return (1);
 	}
 	return (0);
@@ -29,5 +37,5 @@ void	argvparse(char	*argv)
 {
 	if (!dotber(argv))
 		error_dotber(argv);
-	//berparse(argv);
+	// berparse(argv);
 }
