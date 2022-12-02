a48f57bc215fca915c2a1309ec446fa1000965c6
Xabier Matute Rusinol <xmatute-@c3r6s3.42urduliz.com>
2022-12-02 16:40:26 +0100

    quitada una coma de sobra del error .ber

--- Errors/error_dotber.c
+++ Errors/error_dotber.c
@@ -6,7 +6,7 @@
 /*   By: xmatute- <xmatute-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/02 16:22:26 by xmatute-          #+#    #+#             */
-/*   Updated: 2022/12/02 16:24:22 by xmatute-         ###   ########.fr       */
+/*   Updated: 2022/12/02 16:39:23 by xmatute-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,6 @@ int				ft_printf(char const *s, ...);
 
 int	error_dotber(char *argv)
 {
-	ft_printf("Error: %s no es un archivo .ber,", argv);
+	ft_printf("Error: %s no es un archivo .ber", argv);
 	return (-1);
 }
