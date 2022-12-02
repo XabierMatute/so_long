2ada3eb97320cba1d5e1458c319a7f2789ffcb9f
Xabier Matute Rusinol <xmatute-@c3r6s3.42urduliz.com>
2022-12-02 16:44:27 +0100

    main actualizado

--- main.c
+++ main.c
@@ -6,13 +6,19 @@
 /*   By: xmatute- <xmatute-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/01 11:26:47 by xmatute-          #+#    #+#             */
-/*   Updated: 2022/12/01 11:40:42 by xmatute-         ###   ########.fr       */
+/*   Updated: 2022/12/02 16:38:49 by xmatute-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int main(int argc, char const *argv[])
+int				ft_printf(char const *s, ...);
+int	error_argc(int argc);
+void	argvparse(char	*argv);
+
+int main(int argc, char *argv[])
 {
 	if (argc != 2)
-		error_argc();
+		return(error_argc(argc));
+	argvparse(argv[1]);
+	//ft_printf("ey");
 	return 0;
 }
