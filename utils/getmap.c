339f88031a1b7744c6f2a96aa2b3470f138d154e
Xabier Matute Rusinol <xmatute-@c3r6s3.42urduliz.com>
2022-12-02 18:55:18 +0100

    creada la funcion getmap

--- utils/getmap.c
+++ utils/getmap.c
@@ -6,7 +6,30 @@
 /*   By: xmatute- <xmatute-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/02 17:35:04 by xmatute-          #+#    #+#             */
-/*   Updated: 2022/12/02 17:35:05 by xmatute-         ###   ########.fr       */
+/*   Updated: 2022/12/02 18:53:16 by xmatute-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../libft/libft.h"
+#include <fcntl.h>
+#include <unistd.h>
+
+char	*getmap(int fd)
+{
+	char	*c;
+	ssize_t	r;
+	char	*map;
+	size_t	i;
+
+	map = ft_calloc(sizeof(char), mapsize(fd));
+	i = 0;
+	r = read(fd, c, 1);
+	while (r == 1 && *c)
+	{
+		map[i] = *c;
+		r = read(fd, c, 1);
+		i++;
+	}
+	map[i] = 0;
+	return(map);
+}
