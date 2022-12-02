435f0f1c4feadf41d1803b119ccaf255fc54262a
Xabier Matute Rusinol <xmatute-@c3r6s3.42urduliz.com>
2022-12-02 17:48:00 +0100

    parseo del .ber terminado

--- Parse/argvparse.c
+++ Parse/argvparse.c
@@ -6,19 +6,32 @@
 /*   By: xmatute- <xmatute-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/01 15:43:35 by xmatute-          #+#    #+#             */
-/*   Updated: 2022/12/02 17:11:14 by xmatute-         ###   ########.fr       */
+/*   Updated: 2022/12/02 17:46:48 by xmatute-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft/libft.h"
+#include <fcntl.h>
 int	error_dotber(char *argv);
 
-// void	berparse(char *ber)
-// {
-// 	if (!valitber(ber))
-// 		error_invalidber(ber);
-	
-// }
+int	readable(char	*ber)
+{
+	int		fd;
+	char	s[42];
+
+	fd = open(ber, O_RDONLY);
+	if (read(fd, s, 40) < 1)
+		return(0);
+	close(fd);
+	return (1);
+}
+
+void	berparse(char *ber)
+{
+	if (!readable(ber))
+		error_invalidber(ber);
+	parsemap(getmap(ber));
+}
 
 
 int		dotber(char	*argv)
