f295cd137350250f4d132328cb8b03ddaf09dc53
Xabier Matute Rusinol <xmatute-@c3r6s3.42urduliz.com>
2022-12-02 16:43:37 +0100

    terminado el arseo de si es .ber

--- Parse/argvparse.c
+++ Parse/argvparse.c
@@ -6,13 +6,28 @@
 /*   By: xmatute- <xmatute-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/01 15:43:35 by xmatute-          #+#    #+#             */
-/*   Updated: 2022/12/01 16:03:08 by xmatute-         ###   ########.fr       */
+/*   Updated: 2022/12/02 16:35:04 by xmatute-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "../libft/libft.h"
+int	error_dotber(char *argv);
+
+int		dotber(char	*argv)
+{
+	if (argv)
+	{
+		if (ft_strlen(argv) < 4)
+			return (0);
+		if (!ft_strncmp(argv + 4 - ft_strlen(argv), ".ber", 4))
+			return (1);
+	}
+	return (0);
+}
 
 void	argvparse(char	*argv)
 {
 	if (!dotber(argv))
 		error_dotber(argv);
-	berparse(argv);
+	//berparse(argv);
 }
