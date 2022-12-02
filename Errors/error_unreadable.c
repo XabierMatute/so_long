f0326cb0da190d0c12299498fcbaa77e64728544
Xabier Matute Rusinol <xmatute-@c3r6s3.42urduliz.com>
2022-12-02 18:01:13 +0100

    creado /error_unreadable

--- Errors/error_unreadable.c
+++ Errors/error_unreadable.c
@@ -6,7 +6,12 @@
 /*   By: xmatute- <xmatute-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/02 17:50:23 by xmatute-          #+#    #+#             */
-/*   Updated: 2022/12/02 17:50:24 by xmatute-         ###   ########.fr       */
+/*   Updated: 2022/12/02 18:00:27 by xmatute-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+void	error_unreadable(ber)
+{
+	ft_printf("Error: el archivo %s no se ha podido leer\n", ber);
+	exit(-1);
+}
