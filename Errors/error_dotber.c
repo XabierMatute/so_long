5db6bd12b7e8e8301004fd5027ff5d5db8389cb0
Xabier Matute Rusinol <xmatute-@c3r6s3.42urduliz.com>
2022-12-02 17:13:14 +0100

    añadido el exit al error dotber

--- Errors/error_dotber.c
+++ Errors/error_dotber.c
@@ -6,14 +6,15 @@
 /*   By: xmatute- <xmatute-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/02 16:22:26 by xmatute-          #+#    #+#             */
-/*   Updated: 2022/12/02 16:39:23 by xmatute-         ###   ########.fr       */
+/*   Updated: 2022/12/02 16:48:24 by xmatute-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int				ft_printf(char const *s, ...);
+void 			exit(int status);
 
-int	error_dotber(char *argv)
+void	error_dotber(char *argv)
 {
 	ft_printf("Error: %s no es un archivo .ber", argv);
-	return (-1);
+	exit(-1);
 }
