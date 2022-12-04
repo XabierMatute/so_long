3342ba4df07a487bf0fef0975d138522e840b01f
Xabier Matute Rusinol <xmatute-@c3r5s1.42urduliz.com>
2022-12-04 14:16:52 +0100

    creada la funcion validchar

--- Parse/map/invalidchar.c
+++ Parse/map/invalidchar.c
@@ -6,9 +6,24 @@
 /*   By: xmatute- <xmatute-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/04 14:04:16 by xmatute-          #+#    #+#             */
-/*   Updated: 2022/12/04 14:12:02 by xmatute-         ###   ########.fr       */
+/*   Updated: 2022/12/04 14:16:18 by xmatute-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+int	validchar(char c)
+{
+	if (c == '0')
+		return (1);
+	if (c == '1')
+		return (1);
+	if (c == 'C')
+		return (1);
+	if (c == 'E')
+		return (1);
+	if (c == 'P')
+		return (1);
+	return (0);
+}
 
 void	checkinvalidchars(char *map)
 {
