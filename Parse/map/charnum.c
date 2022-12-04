8be975f8d4dd24801829cb4c85493e08b4fd0a57
Xabier Matute Rusinol <xmatute-@c3r5s1.42urduliz.com>
2022-12-04 14:45:36 +0100

    quitado el imput erroneo de error_collectable

--- Parse/map/charnum.c
+++ Parse/map/charnum.c
@@ -6,7 +6,7 @@
 /*   By: xmatute- <xmatute-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/04 14:33:18 by xmatute-          #+#    #+#             */
-/*   Updated: 2022/12/04 14:41:52 by xmatute-         ###   ########.fr       */
+/*   Updated: 2022/12/04 14:45:02 by xmatute-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,5 +32,5 @@ void	checkcharnum(char *map)
 	if (charnum(map, 'P') != 1)
 		error_inipos(charnum(map, 'P'));
 	if (charnum(map, 'C') == 0)
-		error_collectable(charnum(map, 'E'));
+		error_collectable();
 }
