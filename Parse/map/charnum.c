a34656a0cb8bf1ca4b22f80d78ebc7dd9086aaed
Xabier Matute Rusinol <xmatute-@c3r5s1.42urduliz.com>
2022-12-04 14:42:59 +0100

    creada la función checkcharnum

--- Parse/map/charnum.c
+++ Parse/map/charnum.c
@@ -6,7 +6,31 @@
 /*   By: xmatute- <xmatute-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/04 14:33:18 by xmatute-          #+#    #+#             */
-/*   Updated: 2022/12/04 14:33:20 by xmatute-         ###   ########.fr       */
+/*   Updated: 2022/12/04 14:41:52 by xmatute-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+
+unsigned int	charnum(char *map, char c)
+{
+	int	n;
+
+	n = 0;
+	while (*map)
+	{
+		if (*map == c)
+			n++;
+		map++;
+	}
+	return (n);
+}
+
+void	checkcharnum(char *map)
+{
+	if (charnum(map, 'E') != 1)
+		error_exit(charnum(map, 'E'));
+	if (charnum(map, 'P') != 1)
+		error_inipos(charnum(map, 'P'));
+	if (charnum(map, 'C') == 0)
+		error_collectable(charnum(map, 'E'));
+}
