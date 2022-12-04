23db63321adfc6a6126dca3bf0c2b002c249730c
Xabier Matute Rusinol <xmatute-@c3r7s6.42urduliz.com>
2022-12-04 16:11:50 +0100

    creado rectangle.c

--- Parse/map/rectangle.c
+++ Parse/map/rectangle.c
@@ -6,8 +6,29 @@
 /*   By: xmatute- <xmatute-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/04 15:19:52 by xmatute-          #+#    #+#             */
-/*   Updated: 2022/12/04 15:20:48 by xmatute-         ###   ########.fr       */
+/*   Updated: 2022/12/04 16:03:04 by xmatute-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	checkrectangle(char *map);
+#include "../so_long.h"
+
+void	rectangle(char *map)
+{
+	unsigned int	width;
+
+	width = linelen(map);
+	map += width;
+	while (map)
+	{
+		if	(linelen(map) != width)
+			return (0);
+		map += width;
+	}
+	return (1);
+}
+
+void	checkrectangle(char *map)
+{
+	if (!rectangle(map))
+		error_rectangle(map);
+}
