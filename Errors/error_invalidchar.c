f2b585a3528a16450792b18fbe12c95cb35938df
Xabier Matute Rusinol <xmatute-@c3r5s1.42urduliz.com>
2022-12-04 14:23:53 +0100

    creada la funcion error_invalidchar.c

--- Errors/error_invalidchar.c
+++ Errors/error_invalidchar.c
@@ -6,7 +6,15 @@
 /*   By: xmatute- <xmatute-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/04 14:17:27 by xmatute-          #+#    #+#             */
-/*   Updated: 2022/12/04 14:17:28 by xmatute-         ###   ########.fr       */
+/*   Updated: 2022/12/04 14:19:54 by xmatute-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+int				ft_printf(char const *s, ...);
+void 			exit(int status);
+
+void	error_invalidchar(char c)
+{
+	ft_printf("Error\nel mapa contiene el caracter invalido '%c'\n", argv);
+	exit(-1);
+}
