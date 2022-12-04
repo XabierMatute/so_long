4aac2da033a0b3220bbbdb23db9370087eaf0e29
Xabier Matute Rusinol <xmatute-@c3r5s1.42urduliz.com>
2022-12-04 14:24:54 +0100

    corregida una variable

--- Errors/error_invalidchar.c
+++ Errors/error_invalidchar.c
@@ -6,7 +6,7 @@
 /*   By: xmatute- <xmatute-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/04 14:17:27 by xmatute-          #+#    #+#             */
-/*   Updated: 2022/12/04 14:19:54 by xmatute-         ###   ########.fr       */
+/*   Updated: 2022/12/04 14:24:18 by xmatute-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,6 @@ void 			exit(int status);
 
 void	error_invalidchar(char c)
 {
-	ft_printf("Error\nel mapa contiene el caracter invalido '%c'\n", argv);
+	ft_printf("Error\nel mapa contiene el caracter invalido '%c'\n", c);
 	exit(-1);
 }
