183a15a36b6f9e3e4fb816d1e8c20b24fdd35a45
Xabier Matute Rusinol <xmatute-@c3r5s1.42urduliz.com>
2022-12-04 14:01:37 +0100

    añdido es parsemap con sus 5 pasitos

--- Parse/mapparse.c
+++ Parse/mapparse.c
@@ -6,7 +6,15 @@
 /*   By: xmatute- <xmatute-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/02 22:07:45 by xmatute-          #+#    #+#             */
-/*   Updated: 2022/12/02 22:07:46 by xmatute-         ###   ########.fr       */
+/*   Updated: 2022/12/04 14:00:55 by xmatute-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+void	parsemap(char	*map)
+{
+	checkinvalidchars(map);
+	checkcharnum(map);
+	checkrectangle(map);
+	checkclosed(map);
+	checkpath(map);
+}
