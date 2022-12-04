2dd4417334f57e59500dcc0391d2708448840bef
Xabier Matute Rusinol <xmatute-@c3r7s6.42urduliz.com>
2022-12-04 17:19:47 +0100

    progresando en el closed.c

--- Parse/map/closed.c
+++ Parse/map/closed.c
@@ -6,12 +6,40 @@
 /*   By: xmatute- <xmatute-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/04 16:56:09 by xmatute-          #+#    #+#             */
-/*   Updated: 2022/12/04 16:57:59 by xmatute-         ###   ########.fr       */
+/*   Updated: 2022/12/04 17:14:42 by xmatute-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../../so_long.h"
+
+void	error_closed(char	*map);
+
+int	closeud(char *map)
+{
+	unsigned int	width;
+	unsigned int	i;
+
+	i = 0;
+	width = linelen(map);
+	while (i < width)
+	{
+		if (map[i] != '1')
+			return (0);
+		i++;
+	}
+	while (linelen(map + i) && map[i + 1])
+		i++;
+	while (linelen(map + i) < width)
+	{
+		if (map[i] != '1')
+			return (0);
+		i--;
+	}
+	return (1);
+}
+
 void	checkclosed(char *map)
 {
-	if (!closed(map))
+	if (!closeud(map) || !closelr(map))
 		error_closed(map);
 }
