5bd3236ad77002bbb94687beff93a5e41bb18ffd
Xabier Matute Rusinol <xmatute-@c3r5s1.42urduliz.com>
2022-12-09 12:49:14 +0100

    main.c normalizado

--- main.c
+++ main.c
@@ -6,19 +6,20 @@
 /*   By: xmatute- <xmatute-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/01 11:26:47 by xmatute-          #+#    #+#             */
-/*   Updated: 2022/12/06 21:50:15 by xmatute-         ###   ########.fr       */
+/*   Updated: 2022/12/09 12:48:58 by xmatute-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int				ft_printf(char const *s, ...);
-int	error_argc(int argc);
-void	argvparse(char	*argv);
+#include "so_long.h"
 
-int main(int argc, char *argv[])
+int				ft_printf(char const *s, ...);
+int				error_argc(int argc);
+void			argvparse(char	*argv);
+
+int	main(int argc, char *argv[])
 {
 	if (argc != 2)
-		return(error_argc(argc));
+		return (error_argc(argc));
 	argvparse(argv[1]);
-	return 0;
+	return (0);
 }
-
