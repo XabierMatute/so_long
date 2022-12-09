ef73dc9fe7dc371b20adab062da3ea7484d7050f
Xabier Matute Rusinol <xmatute-@c3r5s1.42urduliz.com>
2022-12-09 12:47:55 +0100

    game.c normalizado

--- game.c
+++ game.c
@@ -6,35 +6,55 @@
 /*   By: xmatute- <xmatute-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/07 18:06:30 by xmatute-          #+#    #+#             */
-/*   Updated: 2022/12/08 14:12:26 by xmatute-         ###   ########.fr       */
+/*   Updated: 2022/12/09 12:46:56 by xmatute-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
+int	x_hook(t_mlx *e)
+{
+	gameover(e);
+	return (0);
+}
+
+int	key_hook(int k, t_mlx *e)
+{
+	if (k == Q || k == ESC)
+		gameover(e);
+	if (k == D || k == RK)
+		mr(e);
+	if (k == W || k == UK)
+		mu(e);
+	if (k == SK || k == DK)
+		md(e);
+	if (k == A || k == LK)
+		ml(e);
+	showmap (e);
+	return (0);
+}
+
 void	init(char *m, t_mlx *e)
 {
 	e->mlx = mlx_init();
-	e->w = mlx_new_window(e->mlx, linelen(m) * S, columlen(m) * S, "so_long");
-	e->o = mlx_xpm_file_to_image(e->mlx, "o.xpm", &(e->x), &(e->y));
-	e->l = mlx_xpm_file_to_image(e->mlx, "l.xpm", &(e->x), &(e->y));
-	e->c = mlx_xpm_file_to_image(e->mlx, "c.xpm", &(e->x), &(e->y));
-	e->e = mlx_xpm_file_to_image(e->mlx, "e.xpm", &(e->x), &(e->y));
-	e->p = mlx_xpm_file_to_image(e->mlx, "o.xpm", &(e->x), &(e->y));
+	e->m = m;
+	e->w = mlx_new_window(e->mlx, linelen(m) * S,
+			(columlen(m) + 1) * S, "so_long");
+	e->o = mlx_xpm_file_to_image(e->mlx, "i/o.xpm", &(e->x), &(e->y));
+	e->l = mlx_xpm_file_to_image(e->mlx, "i/l.xpm", &(e->x), &(e->y));
+	e->c = mlx_xpm_file_to_image(e->mlx, "i/c.xpm", &(e->x), &(e->y));
+	e->e = mlx_xpm_file_to_image(e->mlx, "i/e.xpm", &(e->x), &(e->y));
+	e->p = mlx_xpm_file_to_image(e->mlx, "i/p.xpm", &(e->x), &(e->y));
 }
 
 void	game(char *m)
 {
 	t_mlx	e;
-	void	*b;
-	void	*sm;
 
 	init(m, &e);
-
-	mlx_put_image_to_window(e.mlx, e.w, sm, 0, 0);
-	mlx_put_image_to_window(e.mlx, e.w, b, 42, 42);
-	mlx_loop(e.mlx);
-	mlx_put_image_to_window(e.mlx, e.w, b, 69, 69);
+	showmap (&e);
+	mlx_key_hook(e.w, key_hook, &e);
+	mlx_hook(e.w, 17, 1L << 17, x_hook, &e);
 	mlx_loop(e.mlx);
 	mapend(&m);
 }
