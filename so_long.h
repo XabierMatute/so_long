9b7ccfd0310b1acf29047e808b80f15208bd7d3f
Xabier Matute Rusinol <xmatute-@c3r5s1.42urduliz.com>
2022-12-09 12:54:15 +0100

    so_long.h normalizado

--- so_long.h
+++ so_long.h
@@ -6,12 +6,12 @@
 /*   By: xmatute- <xmatute-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/04 15:37:01 by xmatute-          #+#    #+#             */
-/*   Updated: 2022/12/06 19:43:53 by xmatute-         ###   ########.fr       */
+/*   Updated: 2022/12/09 12:53:26 by xmatute-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
+#ifndef SO_LONG_H
+# define SO_LONG_H
 
 # include	<stddef.h>
 # include	<stdlib.h>
@@ -22,18 +22,52 @@
 // # include <stdlib.h>
 // # include <sys/types.h>
 # include "./libft/libft.h"
+# include "mlx/mlx.h"
 
+# define S 42
+# define A 0
+# define W 13
+# define SK 1
+# define D 2
+# define Q 12
+# define ESC 53
+# define LK 123
+# define RK 124
+# define DK 125
+# define UK 126
 
-int		mapsize(int fd);
-char	*getmap(int fd, size_t	size);
-void	mapend(char **map);
-int		linelen(const char *s);
-int		columlen(const char *s);
-char	p(char *m, int x, int y);
-char	*pp(char *m, int x, int y);
-int		x(char *m, char c);
-int		y(char *m, char c);
+typedef struct s_mlx
+{
+	void			*mlx;
+	char			*m;
+	void			*w;
+	int				x;
+	int				y;
+	void			*o;
+	void			*l;
+	void			*c;
+	void			*e;
+	void			*p;
+}					t_mlx;
 
-int		ft_printf(char const *s, ...);
+int				mapsize(int fd);
+char			*getmap(int fd, size_t	size);
+void			mapend(char **map);
+void			showmap(t_mlx *e);
+void			gameover(t_mlx *e);
+unsigned int	charnum(char *map, char c);
+int				linelen(const char *s);
+int				columlen(const char *s);
+char			p(char *m, int x, int y);
+char			*pp(char *m, int x, int y);
+int				x(char *m, char c);
+int				y(char *m, char c);
+
+void			mr(t_mlx *e);
+void			ml(t_mlx *e);
+void			mu(t_mlx *e);
+void			md(t_mlx *e);
+
+int				ft_printf(char const *s, ...);
 
 #endif
